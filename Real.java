732de912c735684cc607d95ab63a91e5dca47853
Luana Coin <106402298+luanacoin@users.noreply.github.com>
2022-11-07 20:15:27 -0300

    Update Real.java

---
+++ Real.java
@@ -0,0 +1,44 @@
+package classes;
+
+import java.util.Objects;
+
+public class Real extends Moeda {
+	
+	//Atributo
+	public Real(double val) {
+		super(val);
+	}
+	
+	//Métodos
+	@Override
+	public void info() {
+		System.out.println("Real: " + getValor());
+		}
+		
+	double total = 0.0;
+	@Override
+	public double converter() {
+		total = getValor();
+		return total;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(total);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Real other = (Real) obj;
+		return Double.doubleToLongBits(total) == Double.doubleToLongBits(other.total);
+	}
+
+}
